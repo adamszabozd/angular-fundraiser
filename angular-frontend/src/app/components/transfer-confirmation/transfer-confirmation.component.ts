ebdc82e2e840d060373e0d42dab7bd048aac440b
katalin_nagy <nagy.katalin.linike@gmail.com>
2021-02-13 17:12:02 +0100

    transfer-confirmation.component.html styled

--- angular-frontend/src/app/components/transfer-confirmation/transfer-confirmation.component.ts
+++ angular-frontend/src/app/components/transfer-confirmation/transfer-confirmation.component.ts
@@ -3,14 +3,19 @@ import {TransferService} from '../../services/transfer.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, Validators} from "@angular/forms";
 import {validationHandler} from "../../utils/validationHandler";
+import {formAppearAnimation} from '../../animations';
 
 @Component({
                selector   : 'app-transfer-confirmation',
                templateUrl: './transfer-confirmation.component.html',
                styleUrls  : ['./transfer-confirmation.component.css'],
+               animations : [
+                   formAppearAnimation,
+               ],
            })
 export class TransferConfirmationComponent implements OnInit {
 
+    state = 'invisible';
     confirmed = false;
     urlCode = false;
     form = this.formBuilder.group({
@@ -24,6 +29,9 @@ export class TransferConfirmationComponent implements OnInit {
     }
 
     ngOnInit() {
+        if (this.state == 'invisible') {
+            setTimeout(() => this.state = 'visible');
+        }
         this.route.paramMap.subscribe(
             paramMap => {
                 const confirmationCode = paramMap.get('code');
