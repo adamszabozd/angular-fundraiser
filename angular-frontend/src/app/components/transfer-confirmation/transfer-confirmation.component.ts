12acd183cacbed4e62d9f489fa68fdffeba1025c
Péterfalvi Ferenc <fpeterfalvi@gmail.com>
2021-02-17 12:25:14 +0100

    translation of previously forgotten components

--- angular-frontend/src/app/components/transfer-confirmation/transfer-confirmation.component.ts
+++ angular-frontend/src/app/components/transfer-confirmation/transfer-confirmation.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, Validators} from "@angular/forms";
 import {validationHandler} from "../../utils/validationHandler";
 import {formAppearAnimation} from '../../animations';
+import {AccountService} from "../../services/account.service";
 
 @Component({
                selector   : 'app-transfer-confirmation',
@@ -25,25 +26,35 @@ export class TransferConfirmationComponent implements OnInit {
     constructor(private transferService: TransferService,
                 private router: Router,
                 private route: ActivatedRoute,
-                private formBuilder: FormBuilder) {
+                private formBuilder: FormBuilder,
+                private accountService: AccountService) {
     }
 
     ngOnInit() {
-        if (this.state == 'invisible') {
-            setTimeout(() => this.state = 'visible');
-        }
-        this.route.paramMap.subscribe(
-            paramMap => {
-                const confirmationCode = paramMap.get('code');
-                if (confirmationCode) {
-                    this.urlCode = true;
-                    this.transferService.confirmTransfer({'confirmationCode': confirmationCode}).subscribe(
-                        () => this.confirmed = true,
+        this.accountService.isLoggedIn().subscribe(
+            loggedIn => {
+                if (!loggedIn) {
+                    this.accountService.loggedInStatusUpdate.next(false);
+                    this.router.navigate(['/login']);
+                } else {
+                    if (this.state == 'invisible') {
+                        setTimeout(() => this.state = 'visible');
+                    }
+                    this.route.paramMap.subscribe(
+                        paramMap => {
+                            const confirmationCode = paramMap.get('code');
+                            if (confirmationCode) {
+                                this.urlCode = true;
+                                this.transferService.confirmTransfer({'confirmationCode': confirmationCode}).subscribe(
+                                    () => this.confirmed = true,
+                                    error => console.warn(error),
+                                );
+                            }
+                        },
                         error => console.warn(error),
                     );
                 }
-            },
-            error => console.warn(error),
+            }
         );
     }
 
