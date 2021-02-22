a8d3f5fcd2dce45929f972683d86e3246b1ae984
katalin_nagy <nagy.katalin.linike@gmail.com>
2021-02-22 11:41:20 +0100

    get daily exchange set to 13:00

--- src/main/java/hu/progmasters/fundraiser/controller/ExchangeController.java
+++ src/main/java/hu/progmasters/fundraiser/controller/ExchangeController.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -30,8 +29,7 @@ public class ExchangeController {
         this.exchangeService = exchangeService;
     }
 
-    @Scheduled(cron = "0 8 19 * * *")
-    @GetMapping
+    @Scheduled(cron = "0 0 12 * * *")
     public void getDailyExchange() {
         RestTemplate restTemplate = new RestTemplate();
         exchangeService.saveDailyRate(Objects.requireNonNull(restTemplate
