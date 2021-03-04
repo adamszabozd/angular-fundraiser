7dcd030e49609387e209ace1b98804d2509898f3
Péterfalvi Ferenc <fpeterfalvi@gmail.com>
2021-03-04 10:18:42 +0100

    BindException handled, FundFormCommandValidator amended

--- src/main/java/hu/progmasters/fundraiser/validation/FundFormCommandValidator.java
+++ src/main/java/hu/progmasters/fundraiser/validation/FundFormCommandValidator.java
@@ -1,5 +1,8 @@
 package hu.progmasters.fundraiser.validation;
 
+import hu.progmasters.fundraiser.domain.Currency;
+import hu.progmasters.fundraiser.domain.FundCategory;
+import hu.progmasters.fundraiser.domain.Status;
 import hu.progmasters.fundraiser.dto.fund.FundFormCommand;
 import hu.progmasters.fundraiser.service.SharedValidationService;
 import org.springframework.stereotype.Component;
@@ -40,18 +43,42 @@ public class FundFormCommandValidator implements Validator {
         if (fund.getShortDescription().replaceAll("\\s", "").length() < 5 || fund.getShortDescription().length() > 250) {
             errors.rejectValue("shortDescription", "short.description.length.wrong");
         }
-        if (fund.getCategory() == null) {
+        if (fund.getCategory() == null || fund.getCategory().length() == 0) {
             errors.rejectValue("category", "category.missing");
+        } else {
+            try {
+                FundCategory.valueOf(fund.getCategory());
+            } catch (IllegalArgumentException e) {
+                errors.rejectValue("category", "category.invalid");
+            }
         }
         if (fund.getTargetAmount() == null) {
             errors.rejectValue("targetAmount", "target.amount.missing");
         } else if (fund.getTargetAmount() <= 0) {
             errors.rejectValue("targetAmount", "target.amount.wrong");
         }
+        if (fund.getCurrency() == null || fund.getCurrency().length() == 0) {
+            errors.rejectValue("currency", "currency.missing");
+        } else {
+            try {
+                Currency.valueOf(fund.getCurrency());
+            } catch (IllegalArgumentException e) {
+                errors.rejectValue("currency", "currency.invalid");
+            }
+        }
         if (fund.getEndDate() != null) {
             LocalDate date = LocalDate.parse(fund.getEndDate());
             if (date.isBefore(LocalDate.now())) {
                 errors.rejectValue("endDate", "end.date.wrong");
+            }
+        }
+        if (fund.getStatus() == null || fund.getStatus().length() == 0) {
+            errors.rejectValue("status", "status.missing");
+        } else {
+            try {
+                Status.valueOf(fund.getStatus());
+            } catch (IllegalArgumentException e) {
+                errors.rejectValue("status", "status.invalid");
             }
         }
     }
